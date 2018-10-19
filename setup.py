64a9026037531a1433ea561caa0ebf0a205bc129
Jani Mikkonen <jani.mikkonen@siili.com>
2018-10-19 09:54:11 +0300

    Misc packaging fixes

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 """
@@ -8,14 +7,32 @@ SeleniumTestability
 import codecs
 from os.path import abspath, dirname, join
 from setuptools import setup, find_packages
+from m2r import parse_from_file
 
 LIBRARY_NAME = 'SeleniumTestability'
 CWD = abspath(dirname(__file__))
 VERSION_PATH = join(CWD, 'src', LIBRARY_NAME, 'version.py')
 exec(compile(open(VERSION_PATH).read(), VERSION_PATH, 'exec'))
 
-with codecs.open(join(CWD, 'README.md'), encoding='utf-8') as reader:
-    LONG_DESCRIPTION = reader.read()
+LONG_DESCRIPTION = parse_from_file(join(CWD, 'README.md'))
+
+with open(join(CWD, 'requirements.txt'), encoding="utf-8") as f:
+    REQUIREMENTS = f.read().splitlines()
+
+
+CLASSIFIERS = '''
+Development Status :: 3 - Alpha
+Topic :: Software Development :: Testing
+Operating System :: OS Independent
+License :: OSI Approved :: Apache Software License
+Programming Language :: Python
+Programming Language :: Python :: 3
+Topic :: Software Development :: Testing
+Framework :: Robot Framework
+Framework :: Robot Framework :: Library
+'''.strip().splitlines()
+
+
 
 setup(
     name='robotframework-%s' % LIBRARY_NAME.lower(),
@@ -27,17 +44,11 @@ setup(
     author='Jani Mikkonen',
     author_email='jani.mikkonen@siili.com',
     license='Apache License 2.0',
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Framework :: Robot Framework',
-        'Topic :: Software Development :: Testing',
-        'License :: OSI Approved :: Apache Software License',
-        'Operating System :: OS Independent'
-    ],
+    classifiers=CLASSIFIERS,
+    install_requires = REQUIREMENTS,
     keywords='robot framework testing automation selenium seleniumlibrary'
              'testability async javascript softwaretesting',
     platforms='any',
     packages=find_packages('src'),
     package_dir={'': 'src'},
-    install_requires=['selenium >= 2.46.1', 'robotframework-seleniumlibrary >= 3.2.0']
 )
