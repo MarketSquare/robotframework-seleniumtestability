5234691367e8d777adfd058bbb19d046fbf68e62
Jani Mikkonen <jani.mikkonen@siili.com>
2018-11-08 17:37:48 +0200

    Fix install and 0.0.5 release

--- setup.py
+++ setup.py
@@ -7,18 +7,20 @@ SeleniumTestability
 import codecs
 from os.path import abspath, dirname, join
 from setuptools import setup, find_packages
-from m2r import parse_from_file
+
+def read_file(filename):
+    buff = None
+    with open(filename,'r',encoding='utf-8') as f:
+        buff = f.read()
+    return buff
 
 LIBRARY_NAME = 'SeleniumTestability'
 CWD = abspath(dirname(__file__))
 VERSION_PATH = join(CWD, 'src', LIBRARY_NAME, 'version.py')
 exec(compile(open(VERSION_PATH).read(), VERSION_PATH, 'exec'))
 
-LONG_DESCRIPTION = parse_from_file(join(CWD, 'README.md'))
-
-with open(join(CWD, 'requirements.txt'), encoding="utf-8") as f:
-    REQUIREMENTS = f.read().splitlines()
-
+LONG_DESCRIPTION = read_file(join(CWD,'README.md'))
+REQUIREMENTS = read_file(join(CWD,'requirements.txt'))
 
 CLASSIFIERS = '''
 Development Status :: 3 - Alpha
@@ -32,14 +34,13 @@ Framework :: Robot Framework
 Framework :: Robot Framework :: Library
 '''.strip().splitlines()
 
-
-
 setup(
     name='robotframework-%s' % LIBRARY_NAME.lower(),
     version=VERSION,
     description='SeleniunTestability library that helps speed up tests with'
                 'asyncronous evens',
     long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
     url='https://github.com/omenia/robotframework-%s' % LIBRARY_NAME.lower(),
     author='Jani Mikkonen',
     author_email='jani.mikkonen@siili.com',
--- src/SeleniumTestability/version.py
+++ src/SeleniumTestability/version.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-VERSION = '0.0.4'
+VERSION = '0.0.5'
 
 
 def get_version():
