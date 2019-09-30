a8bdb62b8c367f0a2c7add36ef74cbc10468d269
Jani Mikkonen <jani.mikkonen@varjo.com>
2019-09-30 14:49:44 +0300

    New keywords to fetch navigator properties
    Note: Get Current Useragent keyword renamed to Get Navigator Useragent
    
    Fixes #14

--- src/SeleniumTestability/plugin.py
+++ src/SeleniumTestability/plugin.py
@@ -381,11 +381,75 @@ class SeleniumTestability(LibraryComponent):
 
     @log_wrapper
     @keyword
-    def get_current_useragent(self: "SeleniumTestability") -> str:
+    def get_navigator_useragent(self: "SeleniumTestability") -> str:
         """
         Returns useragent string of current browser.
         """
-        return self.ctx.driver.execute_script(JS_LOOKUP["useragent"])
+        return self.ctx.driver.execute_script(JS_LOOKUP["navigator"], "userAgent")
+
+    @log_wrapper
+    @keyword
+    def get_navigator_appCodeName(self: "SeleniumTestability") -> str:
+        """
+        Returns appCoedName string of current browser.
+        """
+        return self.ctx.driver.execute_script(JS_LOOKUP["navigator"], "appCodeName")
+
+    @log_wrapper
+    @keyword
+    def get_navigator_appname(self: "SeleniumTestability") -> str:
+        """
+        Returns appName string of current browser.
+        """
+        return self.ctx.driver.execute_script(JS_LOOKUP["navigator"], "appName")
+
+    @log_wrapper
+    @keyword
+    def get_navigator_appversion(self: "SeleniumTestability") -> str:
+        """
+        Returns appVersion string of current browser.
+        """
+        return self.ctx.driver.execute_script(JS_LOOKUP["navigator"], "appVersion")
+
+    @log_wrapper
+    @keyword
+    def get_navigator_cookieenabled(self: "SeleniumTestability") -> bool:
+        """
+        Returns cookieEnabled boolean of current browser.
+        """
+        return self.ctx.driver.execute_script(JS_LOOKUP["navigator"], "cookieEnabled")
+
+    @log_wrapper
+    @keyword
+    def get_navigator_language(self: "SeleniumTestability") -> str:
+        """
+        Returns language string of current browser.
+        """
+        return self.ctx.driver.execute_script(JS_LOOKUP["navigator"], "language")
+
+    @log_wrapper
+    @keyword
+    def get_navigator_online(self: "SeleniumTestability") -> bool:
+        """
+        Returns online boolean of current browser.
+        """
+        return self.ctx.driver.execute_script(JS_LOOKUP["navigator"], "onLine")
+
+    @log_wrapper
+    @keyword
+    def get_navigator_platform(self: "SeleniumTestability") -> str:
+        """
+        Returns platform string of current browser.
+        """
+        return self.ctx.driver.execute_script(JS_LOOKUP["navigator"], "platform")
+
+    @log_wrapper
+    @keyword
+    def get_navigator_product(self: "SeleniumTestability") -> str:
+        """
+        Returns product string of current browser.
+        """
+        return self.ctx.driver.execute_script(JS_LOOKUP["navigator"], "product")
 
     @log_wrapper
     @keyword
